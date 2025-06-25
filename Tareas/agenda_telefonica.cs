2f72abfd1682695887d15041023822fd14a4539e
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-24 19:19:10 -0500

    clase agenda telefonica y atrbiutos

--- Tareas/agenda_telefonica.cs
+++ Tareas/agenda_telefonica.cs
@@ -1,6 +1,11 @@
 //creacion de la clase y sus atributos
 public class agenda_telefonica
 {
- 
+    public string Nombres { get; set; }
+    public string Telefono { get; set; }
+    public int Edad { get; set; }
+    public string Correo { get; set; }
+    public string Direccion { get; set; }
+
 
 }
